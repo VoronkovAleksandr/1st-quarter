676f4c4f72308c7d4af941ca6bedbe205e3a2b81
VoronkovAleksandr <a_voron@list.ru>
2023-01-28 16:32:54 +0500

    Progtamm complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void ArrayOutput(string[] arr)
+{
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+void TransferArray(string[] array1, string[] array2, int minLength)
+{
+    int count=0;
+    int lenghtElement = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        lenghtElement =  array1[i].Length;
+        if (lenghtElement<=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }  
+}
+
+int CountingLenElements (string[] array,int minLength)
+{
+    int count = 0;
+    int lenghtElement = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        lenghtElement =  array[i].Length;
+        if (lenghtElement<=3)
+        {
+            count++;
+        }
+    }  
+    return count;
+}
+
+string[] ArrayEntry(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = DataEntry("Введите значение: ");
+    }
+    return array;
+}
+
+string DataEntry(string text)
+{
+    Console.Write(text);
+    string result = Console.ReadLine();
+    return result;
+}
+
+int quantity = Convert.ToInt32(DataEntry("Введите количество элементов массива: "));
+string[]array = new string[quantity];
+array = ArrayEntry(array);
+int j = CountingLenElements(array,3);
+string[] resultArray = new string[j];
+TransferArray(array, resultArray, 3);
+
+ArrayOutput(array);
+ArrayOutput(resultArray);
